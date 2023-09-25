25172799b898ace84a0ef7062b76cd3fc0fc105a
Wale <Adewale.megbabi@gmail.com>
2023-09-25 21:05:04 +0100

    Initial commit

--- 01-resources/main.tf
+++ 01-resources/main.tf
@@ -1,16 +1,17 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-02ce98398a09666eb"]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "frontend"
   }
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "frontend" {
   zone_id = "Z08999912AI7EUJ47AGDO"
-  name    = "test.waleapagun.online"
+  name    = "frontend.waleapagun.online"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.web.private_ip]
+  records = [aws_instance.frontend.private_ip]
 }
