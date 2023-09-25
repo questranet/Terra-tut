8946ebe5ba7430406617280987f074cdc13a66d5
Wale <Adewale.megbabi@gmail.com>
2023-09-25 20:12:23 +0100

    Initial commit

--- 01-resources/main.tf
+++ 01-resources/main.tf
@@ -6,3 +6,11 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z08999912AI7EUJ47AGDO"
+  name    = "test.waleapagun.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.web.private_ip]
+}
