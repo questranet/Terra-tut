c9d27abc0651b913d4c8ffa270845c39edd50925
Wale <Adewale.megbabi@gmail.com>
2023-09-26 10:57:27 +0100

    Initial commit

---
+++ expense-v1/main.tf
@@ -0,0 +1,53 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-02ce98398a09666eb"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z08999912AI7EUJ47AGDO"
+  name    = "frontend.waleapagun.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "backend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-02ce98398a09666eb"]
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_route53_record" "backend" {
+  zone_id = "Z08999912AI7EUJ47AGDO"
+  name    = "backend.waleapagun.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.backend.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-02ce98398a09666eb"]
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z08999912AI7EUJ47AGDO"
+  name    = "mysql.waleapagun.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
