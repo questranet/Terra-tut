7c77308d7da33b0799d234dec1ad11b04f64db0e
Wale <Adewale.megbabi@gmail.com>
2023-11-17 22:24:46 +0100

    Initial commit

--- 01-resources/main.tf
+++ 01-resources/main.tf
@@ -1,12 +1,12 @@
-#resource "aws_instance" "web" {
-#  ami           = "ami-03265a0778a880afb"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "HelloWorld"
-#  }
-#}
-#
+resource "aws_instance" "web" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
 #resource "aws_route53_record" "www" {
 #  zone_id = z
 #  name    = "test.rdevopsb73.online"
